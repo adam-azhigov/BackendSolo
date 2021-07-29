454f72733856e1b1c49ee3cce9059bbf4d6404f3
azhigov <adam.azhigov97@gmail.com>
2021-07-29 18:00:49 +0300

    delete client

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(morgan('combined'))
 app.use(express.static(path.resolve(__dirname, "client", "build")));
+
 app.use(router)
 
 app.get('*', (req, res) => {
