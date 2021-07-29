e2af865875524b69add648d2fbf65015c37d60ff
azhigov <adam.azhigov97@gmail.com>
2021-07-29 17:42:49 +0300

    add

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.use(router)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  res.sendFile(path.resolve(__dirname, "client/build/index.html"))
 })
 
 async function start () {
